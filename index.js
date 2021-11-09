8440b66752cc9dd57b377bf8ad669e87c22f1fba
Md Tariqul Islam <tariqul15-12963@diu.edu.bd>
2021-11-09 21:48:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const admin = require("firebase-admin");
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000
+
+// 
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgnum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers?.authorization?.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token)
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal')
+        const appoinmentsCollection = database.collection('appoinments')
+        const usersCollection = database.collection('users')
+
+        app.get('/appoinments', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email, date: date };
+
+            const cursor = appoinmentsCollection.find(query)
+            const appoinments = await cursor.toArray();
+            res.json(appoinments);
+        })
+
+        app.post('/appoinments', async (req, res) => {
+            const appoinment = req.body;
+            const result = await appoinmentsCollection.insertOne(appoinment);
+            res.json(result);
+
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.body;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester })
+                if (requesterAccount.role === 'admin') {
+
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have access to make admin' })
+            }
+        })
+    }
+    finally {
+        //  await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors Portal')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
